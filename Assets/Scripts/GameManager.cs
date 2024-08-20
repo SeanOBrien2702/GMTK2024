fda7b6129a7d33a9541bc9e08f6e30e04a8cfbcb
SeanOBrien2702 <seanobrien2702@gmail.com>
2024-08-20 02:35:08 -0400

    Added timer and money UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static event EventHandler OnRoundOver;
+    public static event EventHandler<OnMoneyChangedEvent> OnMoneyChanged;
+    public class OnMoneyChangedEvent : EventArgs
+    {
+        public int Money;
+    }
+    public static event EventHandler<OnGameTimeChangedEvent> OnGameTimeChanged;
+    public class OnGameTimeChangedEvent : EventArgs
+    {
+        public float Time;
+    }
     public static GameManager Instance;
     PlateKitchenObject scaledPlate;
     bool isGamePaused = false;
     int level = 0;
+    int money;
 
+    float roundTime = 180;
+    float roundTimer = 0;
+
+    public float RoundTimer
+    {
+        get => roundTimer;
+        set
+        {
+            roundTimer = value;
+            RoundTimer_OnValueChanged();
+        }
+    }
 
 
     public bool IsGamePaused { get => isGamePaused; set => isGamePaused = value; }
@@ -31,6 +54,24 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        RoundTimer += Time.deltaTime;
+        if(roundTimer > roundTime)
+        {
+            OnRoundOver?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    private void RoundTimer_OnValueChanged()
+    {
+        OnGameTimeChanged?.Invoke(this, new OnGameTimeChangedEvent
+        {
+            Time = roundTimer / roundTime
+        });
+    }
+
+    public void StartNextRound()
+    {
+        level++;
+        roundTime = 0;
     }
 }
